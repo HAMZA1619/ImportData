8e0d366ab39f9f064e4be5cfa1d7596bb14a6988
hamza elglaoui <chyoin188@gmail.com>
2022-07-13 14:18:55 +0000

    fix empty csv

--- ImportData-controller.js
+++ ImportData-controller.js
@@ -9,15 +9,18 @@ module.exports.create = async function (req, res) {
   try {
     const importArray = await csv().fromString(req.file.buffer.toString());
     importArray.forEach(async (row) => {
-      const oldpost = await ImportData.findOne({
-        post_id: Number(row["post id"]),
-      });
-      if (oldpost) {
-        await saveRelated(row, oldpost._id);
-        await updatePost(row, oldpost._id);
-      } else {
-        let id = await savePost(row);
-        await saveRelated(row, id);
+      let exist = row["post id"]
+      if (exist) {
+        const oldpost = await ImportData.findOne({
+          post_id: Number(row["post id"]),
+        });
+        if (oldpost) {
+          await saveRelated(row, oldpost._id);
+          await updatePost(row, oldpost._id);
+        } else {
+          let id = await savePost(row);
+          await saveRelated(row, id);
+        }
       }
     });
     res.send(importArray);
@@ -30,10 +33,10 @@ async function savePost(data) {
   try {
     let post = {};
     post["page_id"] = await data["page id"];
-    post["post_id"] = Number(await data["post id"]);
+    post["post_id"] = Number(data["post id"]);
     post["ad_format"] = await data["ad format"];
-    post["post_create"] = await data["post created"].split("/").join("-") || '2020-10-10';
-    post["last_seen"] = new Date().toLocaleDateString().split("/").join("-");
+    post["post_create"] = (new Date(await data["post created"]).toLocaleDateString("en-CA")) || "2020-10-10";
+    post["last_seen"] = new Date().toLocaleDateString("en-CA");
     post["intersted"] = await data["intersted"];
     post["ender"] = await data["ender"];
     post["age"] = await data["age"];
@@ -43,14 +46,14 @@ async function savePost(data) {
     post["t_lang"] = await data["language = for_target_audience"];
     post["page_link"] = await data["page link"];
     post["button"] = await data["Button"];
-    post["share"] = Number(await data["shares number"]);
-    post["comment"] = Number(await data["comment number"]);
-    post["like"] = Number(await data["likes number"]);
+    post["share"] = Number((await data["shares number"]) || "0");
+    post["comment"] = Number((await data["comment number"]) || "0");
+    post["like"] = Number((await data["likes number"]) || "0");
     post["page_name"] = await data["facebook page name"];
-    post["Ad_Description"] =await data["Ad Description"];
-    post["title"] =await data["Ad Title"];
-    post["category"] = JSON.stringify(await data["category"].split(","));
-    post["keywords"] =await data["keywords"];
+    post["Ad_Description"] = await data["Ad Description"];
+    post["title"] = await data["Ad Title"];
+    post["category"] = data["category"] ? JSON.stringify(await data["category"].split(",") ) : null ;
+    post["keywords"] = await data["keywords"];
     post["resource"] = await Resource(data);
     const postAdded = await ImportData.create(post);
     return postAdded._id.toString();
@@ -74,10 +77,7 @@ async function saveRelated(data, id) {
 async function updatePost(data, id) {
   try {
     let updatePost = {};
-    updatePost["last_seen"] = new Date()
-      .toLocaleDateString()
-      .split("/")
-      .join("-");
+    updatePost["last_seen"] = new Date().toLocaleDateString("en-CA");
     updatePost["share"] = Number(data["shares number"]);
     updatePost["comment"] = Number(data["comment number"]);
     updatePost["like"] = Number(data["likes number"]);
@@ -107,7 +107,7 @@ async function uploadfile(link, extension) {
           data.push(chunk);
         });
         response.on("end", function () {
-          fs.writeFileSync(__dirname+"/uploads/"+name, data.read());
+          fs.writeFileSync(__dirname + "/uploads/" + name, data.read());
         });
       })
       .end();
