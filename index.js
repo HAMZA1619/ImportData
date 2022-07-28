bd730db13a24244b0ee67e41701afdf485bf5ff0
hamza elglaoui <chyoin188@gmail.com>
2022-07-28 12:04:48 +0000

    access logs

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const express = require("express");
-var multer = require('multer');
-var upload = multer();
+let multer = require("multer");
+let upload = multer();
 const mongoose = require("mongoose");
-
+const fs = require("fs");
 const { create } = require("./ImportData-controller");
+const morgan = require('morgan')
 
 require("dotenv").config();
-
+const accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
 const app = express();
-
 mongoose
   .connect(process.env.MONGO_DB, { useNewUrlParser: true })
   .then((e) => {
     console.log("db conneted");
   })
   .catch((e) => console.log(e));
-app.use(upload.single('file')); 
+app.use(upload.single("file"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', function (req, res) {
-  res.send('Welcome');
+app.get("/", function (req, res) {
+  res.send("Welcome");
 });
-
+app.use(morgan('combined', {stream: accessLogStream}))
 app.use("/bots/import_data", create);
+
 
 app.listen(process.env.PORT, () =>
   console.log(`server running in ${process.env.PORT} `)
